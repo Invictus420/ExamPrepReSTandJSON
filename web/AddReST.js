3a215aa97e32dc68731399cf5e8bb1a74a71f15e
Invictus420 <alexandar_slambert@hotmail.com>
2016-06-13 01:31:36 +0200

    stuffs

--- web/AddReST.js
+++ web/AddReST.js
@@ -8,7 +8,13 @@ angular.module('myApp', [
         .controller('myCtrl', function ($http, $scope, NgTableParams) {
             $scope.calculate = function () {
                 $http({method: 'GET',
+                    Access: "http://exam-safevand.rhcloud.com/epReSTandJSON/",
+                    Control: "http://exam-safevand.rhcloud.com/epReSTandJSON/",
+                    Allow: "http://exam-safevand.rhcloud.com/epReSTandJSON/",
+                    Origin:"http://exam-safevand.rhcloud.com/epReSTandJSON/",
+//                    Access-Control-Allow-Origin: "http://exam-safevand.rhcloud.com/epReSTandJSON/",
                     url: "api/addresses/" + $scope.num + "/" + $scope.props
+                     
                 }).then(function(response){
                      var data = response.data;
                      
